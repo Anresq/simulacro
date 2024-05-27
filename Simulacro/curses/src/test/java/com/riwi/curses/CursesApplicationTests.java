9c1fe52dc9b02fc7593e61bad6f8a80d26eb56e9
Anresq <147433149+Anresq@users.noreply.github.com>
2024-05-27 09:24:22 -0500

    initializer

---
+++ Simulacro/curses/src/test/java/com/riwi/curses/CursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.curses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
